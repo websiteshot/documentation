a97593c6cbbbfc4f6d284f152b49337311a3b705
Adam <i@adamurban.de>
2020-12-16 19:27:52 +0100

    update structure

--- sidebars.js
+++ sidebars.js
@@ -2,23 +2,20 @@ module.exports = {
   docs: {
     Websiteshot: ["websiteshot/introduction", "websiteshot/getting-started"],
     Guides: ["guides/simple-script"],
-    API: [
-      "api/introduction",
-      {
-        Projects: ["api/projects/create-screenshot-job"],
-        Screenshots: ["api/screenshots/get-screenshots"],
-      },
-      {
-        Types: [
-          "api/types/job-result",
-          "api/types/login-parameter",
-          "api/types/meta",
-          "api/types/screenshot-parameter",
-          "api/types/url-config",
-          "api/types/shadow-options",
-          "api/types/color",
-        ],
-      },
+    API: [],
+  },
+  api: {
+    "Getting Started": ["api/introduction"],
+    Projects: ["api/projects/create-screenshot-job"],
+    Screenshots: ["api/screenshots/get-screenshots"],
+    Types: [
+      "api/types/job-result",
+      "api/types/login-parameter",
+      "api/types/meta",
+      "api/types/screenshot-parameter",
+      "api/types/url-config",
+      "api/types/shadow-options",
+      "api/types/color",
     ],
   },
 };
